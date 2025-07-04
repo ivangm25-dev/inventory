05d3192a5f8f992e3222746ca7b0b8b39456886b
ivangm25-dev <ivangm2511@gmail.com>
2025-07-03 21:50:57 -0600

    Update Test

--- src/test/java/com/wigm/inventoryControl/integration/inventory.java
+++ src/test/java/com/wigm/inventoryControl/integration/inventory.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.wigm.inventoryControl.repository.ProductRepository;
 import com.wigm.inventoryControl.repository.entities.Inventory;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +15,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,12 +23,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class inventory {
     @Autowired
     private MockMvc mockMvc;
-
+    @Mock
     private ProductRepository productRepository;
-
-    inventory(){
-        this.productRepository = mock(ProductRepository.class);
-    }
 
     @Test
     public void getAllProducts() throws Exception {
@@ -54,13 +49,13 @@ public class inventory {
     @Test
     public void createProduct() throws Exception {
         Inventory inventory = new Inventory();
-        inventory.setItem("example");
+        inventory.setItem("example1");
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
         ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
         String requestJson=ow.writeValueAsString(inventory);
 
-        when(productRepository.insert(inventory)).thenReturn(inventory);
+        doReturn(inventory).when(productRepository).insert(inventory);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/inventory")
                         .contentType(MediaType.APPLICATION_JSON).content(requestJson))
@@ -73,13 +68,13 @@ public class inventory {
     @Test
     public void updateProduct() throws Exception {
         Inventory inventory = new Inventory();
-        inventory.setItem("example");
+        inventory.setItem("example2");
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
         ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
         String requestJson=ow.writeValueAsString(inventory);
 
-        when(productRepository.save(inventory)).thenReturn(inventory);
+        doReturn(inventory).when(productRepository).save(inventory);
 
         mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/inventory")
                         .contentType(MediaType.APPLICATION_JSON).content(requestJson))
@@ -92,13 +87,13 @@ public class inventory {
     @Test
     public void deleteProduct() throws Exception {
         Inventory inventory = new Inventory();
-        inventory.setItem("example");
+        inventory.setItem("example1");
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
         ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
         String requestJson=ow.writeValueAsString(inventory);
-        ProductRepository repository = Mockito.spy(ProductRepository.class);
-        doNothing().when(repository).deleteById("example");
+
+        doNothing().when(productRepository).deleteById("example1");
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/inventory?id=example")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
